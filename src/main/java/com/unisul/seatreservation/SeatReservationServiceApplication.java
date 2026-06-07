8523f1b29d78fd9aa6dcfcb3f94ec0d58b5e79a1
jjulinha <jodjulia03@gmail.com>
2026-06-07 09:30:00 -0300

    chore: setup inicial do projeto via Spring Initializr

---
+++ src/main/java/com/unisul/seatreservation/SeatReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.unisul.seatreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatReservationServiceApplication.class, args);
+	}
+
+}
